8771fb96047f627425734c8c9b7fc008daa0b37a
Jithin <jithinka2323@gmail.com>
2025-06-13 23:44:47 +0530

    Initial commit: setup basic express server with TypeORM and CORS

---
+++ app.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import dataSource from "./db/data-source";
+import cors from "cors"
+
+const server = express();
+
+server.use(express.json());
+server.use( cors() )
+
+server.get("/", (req, res) => {
+    console.log(req.url);
+    res.status(200).send("Hello world - Recruitment Platform Backend");
+});
+
+(async () => {
+    try {
+        await dataSource.initialize();
+        console.log("connected");
+
+        server.listen(3000, () => {
+            console.info("server listening to 3000");
+        });
+    } catch (e) {
+        console.error(`Failed to connect to DB:  ${e}`);
+        process.exit(1);
+    }
+})();
---
+++ db/data-source.ts
@@ -0,0 +1,22 @@
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import 'dotenv/config'
+
+const dataSource = new DataSource({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_DATABASE,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,    
+    extra: {max: 5, min: 2},
+    synchronize: false,
+    logging: false,
+    namingStrategy: new SnakeNamingStrategy,
+    entities: ["dist/entities/*.js"],
+    migrations: ["dist/db/migrations/*.js"]
+
+})
+
+export default dataSource;
