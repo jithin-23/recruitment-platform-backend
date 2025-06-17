f4c4d8f3eb582f943e77d8d1e99521dc2dea6cd5
Sooraj S <sooraj2642@gmail.com>
2025-06-17 12:16:38 +0530

    Implement bonus status patch functionality

--- routes/bonus.routes.ts
+++ routes/bonus.routes.ts
@@ -4,11 +4,11 @@ import Bonus from "../entities/bonus.entity";
 import BonusRepository from "../repositories/bonus.repository";
 import BonusService from "../services/bonus.service";
 import BonusController from "../controllers/bonus.controller";
-
+import { referralService } from "../routes/referral.route";
 const bonusRouter = express.Router();
 
 const bonusRepository = new BonusRepository(dataSource.getRepository(Bonus));
-const bonusService = new BonusService(bonusRepository);
+const bonusService = new BonusService(bonusRepository, referralService);
 const bonusController = new BonusController(bonusService, bonusRouter);
 
 export default bonusRouter;
